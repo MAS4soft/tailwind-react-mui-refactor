6f4bcabdbbfc8f2d0fd34b9db9470502d4f05762
MAS4soft <mas4soft@gmail.com>
2024-09-17 14:51:10 +0200

    firebase try

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-  // Other Vite configuration options
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg'], // Include image file types
-
+  base: '/', 
+  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.svg'], // تضمين أنواع ملفات الصور
 })
